d94e5ce420bab8ad06a2fb4a383e585306d036f1
BARNETT <db0958392@otc.edu>
2022-12-06 09:29:16 -0600

    first commit and done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <NavBar></NavBar>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <CardComponent class="col"  v-for="movie in movies" v-bind:key="movie.id" v-bind:amovie="movie"></CardComponent>
+    </div>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CardComponent from './components/CardComponent.vue';
+import NavBar from './components/NavBar.vue'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    CardComponent
+  },
+  data(){
+    return{
+      movies:[]
+    }
+  },
+  mounted(){
+    axios
+    .get("https://api.themoviedb.org/3/movie/now_playing?api_key=df78dbf203b1cd1f3a6d506adc95f2fd&language=en-US&page=1")
+    .then((response) => {
+      this.movies = response.data.results.slice(0,4);
+    })
   }
 }
 </script>
@@ -21,8 +41,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img :src=imgPath class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{amovie.name}}</h5>
+            <p class="card-text">{{amovie.overview}}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default({
+        name: 'CardComponent',
+        props:['amovie'],
+        data(){
+            return {
+                imgPath: "https://image.tmdb.org/t/p/w500/" + this.amovie.poster_path
+            }
+        }
+    })
+</script>
